e8e8be3b588cc42f0ad289d202c2864de6f568e0
Crypto-zombie <volcano103@gmail.com>
2023-04-25 10:54:36 +0300

    chore: resize material

--- src/componentsFor3D/BasicModel/index.tsx
+++ src/componentsFor3D/BasicModel/index.tsx
@@ -14,7 +14,7 @@ const BasicModel = () => {
   // rest of your component code here
   return (
     <mesh position={[0, 0.4, 0.07]}>
-      <planeBufferGeometry args={[2.1, 2.1, 1]} />
+      <planeBufferGeometry args={[2.18, 2.1, 1]} />
       <meshStandardMaterial
         map={texture}
         lightMap={texture}
